afdb8250df20867f7a8c42ec379f4261c26f1a40
Asahi <asahi@kadefi.money>
2022-12-09 18:59:16 +0800

    Added link to portfolio for sidebar

--- apps/dexscan/src/layouts/Layout.tsx
+++ apps/dexscan/src/layouts/Layout.tsx
@@ -1,7 +1,9 @@
 import { Dialog, Transition } from "@headlessui/react";
 import {
+  ArrowTopRightOnSquareIcon,
   Bars3BottomLeftIcon,
   MagnifyingGlassIcon,
+  WalletIcon,
 } from "@heroicons/react/20/solid";
 import { HeartIcon, HomeIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
@@ -23,6 +25,12 @@ import { SearchModal } from "../components/SearchModal";
 const navigation = [
   { name: "Dashboard", href: "/", icon: HomeIcon },
   { name: "Support Us", href: "/donate", icon: HeartIcon },
+  {
+    name: "Portfolio",
+    href: "https://kadefi.money",
+    icon: WalletIcon,
+    isExternalLink: true,
+  },
 ];
 
 type Props = {
@@ -72,6 +80,48 @@ const Layout = (props: Props) => {
     </div>
   );
 
+  const getNavMenuItems = () =>
+    navigation.map((item) => {
+      const navMenu = (
+        <div
+          className={clsx(
+            router.pathname === item.href
+              ? "bg-slate-100 text-slate-900"
+              : "text-slate-600 hover:bg-slate-50 hover:text-slate-900",
+            "group mb-3 flex cursor-pointer items-center rounded-md px-2 py-2 text-sm font-medium transition"
+          )}
+        >
+          <item.icon
+            className={clsx(
+              router.pathname === item.href
+                ? "text-slate-500"
+                : "text-slate-400 group-hover:text-slate-500",
+              "mr-4 h-6 w-6 flex-shrink-0"
+            )}
+            aria-hidden="true"
+          />
+          <div className="truncate">{item.name}</div>
+          {item.isExternalLink && (
+            <ArrowTopRightOnSquareIcon className="ml-2 h-4 w-4" />
+          )}
+        </div>
+      );
+
+      if (item.isExternalLink) {
+        return (
+          <a key={item.name} href={item.href} target="_blank" rel="noreferrer">
+            {navMenu}
+          </a>
+        );
+      }
+
+      return (
+        <Link key={item.name} href={item.href}>
+          {navMenu}
+        </Link>
+      );
+    });
+
   const mobileSideBar = (
     <Transition.Root show={sidebarOpen} as={Fragment}>
       <Dialog
@@ -88,7 +138,7 @@ const Layout = (props: Props) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-600 bg-opacity-20 backdrop-blur" />
+          <div className="fixed inset-0 bg-slate-600 bg-opacity-20 backdrop-blur" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-40 flex">
@@ -129,32 +179,7 @@ const Layout = (props: Props) => {
                 {logo}
               </div>
               <div className="mt-5 h-0 flex-1 overflow-y-auto">
-                <nav className="space-y-1 px-2">
-                  {navigation.map((item) => (
-                    <Link key={item.name} href={item.href}>
-                      <div
-                        onClick={() => setSidebarOpen(false)}
-                        className={clsx(
-                          router.pathname === item.href
-                            ? "bg-gray-100 text-gray-900"
-                            : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
-                          "group flex cursor-pointer items-center rounded-md px-2 py-2 text-base font-medium"
-                        )}
-                      >
-                        <item.icon
-                          className={clsx(
-                            router.pathname === item.href
-                              ? "text-gray-500"
-                              : "text-gray-400 group-hover:text-gray-500",
-                            "mr-4 h-6 w-6 flex-shrink-0"
-                          )}
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </div>
-                    </Link>
-                  ))}
-                </nav>
+                <nav className="px-2">{getNavMenuItems()}</nav>
               </div>
             </Dialog.Panel>
           </Transition.Child>
@@ -167,7 +192,7 @@ const Layout = (props: Props) => {
   const mobileSideBarToggle = (
     <button
       type="button"
-      className="border border-slate-800 px-3 text-gray-500 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white md:hidden md:px-4"
+      className="border border-slate-800 px-3 text-slate-500 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white md:hidden md:px-4"
       onClick={() => setSidebarOpen(true)}
     >
       <span className="sr-only">Open sidebar</span>
@@ -182,31 +207,7 @@ const Layout = (props: Props) => {
           {logo}
         </div>
         <div className="mt-4 flex flex-grow flex-col">
-          <nav className="flex-1 space-y-1 px-2 pb-4">
-            {navigation.map((item) => (
-              <Link key={item.name} href={item.href}>
-                <div
-                  className={clsx(
-                    router.pathname === item.href
-                      ? "bg-gray-100 text-gray-900"
-                      : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
-                    "group mb-2 flex cursor-pointer items-center rounded-md px-2 py-2 text-sm font-medium transition"
-                  )}
-                >
-                  <item.icon
-                    className={clsx(
-                      router.pathname === item.href
-                        ? "text-gray-500"
-                        : "text-gray-400 group-hover:text-gray-500",
-                      "mr-4 h-6 w-6 flex-shrink-0"
-                    )}
-                    aria-hidden="true"
-                  />
-                  <div className="truncate">{item.name}</div>
-                </div>
-              </Link>
-            ))}
-          </nav>
+          <nav className="flex-1 px-2 pb-4">{getNavMenuItems()}</nav>
         </div>
       </div>
     </div>
